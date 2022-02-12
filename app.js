8d935e0aa005cefd6c95dbb66f322fec44282c66
Prokash Pul <pprokash102@gmail.com>
2022-02-12 21:09:23 +0600

    called a function and add discrement valu

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
+function valueIncrease(isIncrease) {
+    const inputValue = document.getElementById('input-id');
+    const inputCount = Number(inputValue.value);
+    if (isIncrease) {
+        inputValue.value = inputCount + 1;
+    } else {
+        if (inputValue.value > 0) {
+            inputValue.value = inputCount - 1;
+        }
+    }
+
+}
+
 // plus button add event listener 
 
 document.getElementById('add').addEventListener('click', function () {
-    const inputValue = document.getElementById('input-id');
-    const inputCount = Number(inputValue.value);
-    inputValue.value = inputCount + 1;
+    valueIncrease(true);
+});
+
+document.getElementById('minus').addEventListener('click', function () {
+    valueIncrease(false);
 });
