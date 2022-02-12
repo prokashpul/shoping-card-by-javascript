5793726380659382fe67c15b9764b8c8da19563c
Prokash Pul <pprokash102@gmail.com>
2022-02-12 21:16:04 +0600

    product price value update

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 function valueIncrease(isIncrease) {
     const inputValue = document.getElementById('input-id');
-    const inputCount = Number(inputValue.value);
+    let inputCount = Number(inputValue.value);
     if (isIncrease) {
-        inputValue.value = inputCount + 1;
+        inputCount = inputCount + 1;
     } else {
         if (inputValue.value > 0) {
-            inputValue.value = inputCount - 1;
+            inputCount = inputCount - 1;
         }
     }
-
+    inputValue.value = inputCount;
+    // up price and down price 
+    const updatePrice = document.getElementById('update-price');
+    updatePrice.innerText = 59 * inputValue.value;
 }
 
 // plus button add event listener 
